53c677beb168480abe557285d11d54a6d924bf41
mansooriabbas <mansoori.abbas@outlook.com>
2024-02-07 00:53:39 +0100

    rendered characters to dom

--- script.js
+++ script.js
@@ -3,26 +3,30 @@ const infoList = document.querySelector(".right__info-list");
 
 const baseUrl = "https://swapi.dev/api/";
 
-
 const fetchApi = async (endpoint) => {
-    try {
-        const res = await fetch(baseUrl + endpoint);
-        const data = await res.json();
-        return data.results
-    } catch (err) {
-        console.error(`Error fetching data for ${endpoint}: ${err}`);
-    }
+  try {
+    const res = await fetch(baseUrl + endpoint);
+    const data = await res.json();
+    return data.results;
+  } catch (err) {
+    console.error(`Error fetching data for ${endpoint}: ${err}`);
+  }
 };
- 
+
+let character = "";
 
 const renderCharacters = async () => {
-    const people = await fetchApi("people/");
-    let html = ""
-    people.forEach((person) => {
-        html += `<li>${person.name}</li>`
-    })
-    console.log(html);
-    characterList.innerHTML = html
+  const people = await fetchApi("people/");
+  people.forEach((person) => {
+    character += `<li>${person.name}</li>`;
+    console.log(person);
+  });
+
+  characterList.innerHTML = character;
 };
 
-renderCharacters()
+renderCharacters();
+
+characterList.addEventListener("click",(e) => {
+    console.log(e.target);
+})
